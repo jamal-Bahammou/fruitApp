dc2ba9847d51448a6ff1b1d2ea1e2239e240f807
Jamal Bahammou <jamalbahammou456@gmail.com>
2019-07-30 00:10:53 +0100

    Updates

--- index.js
+++ index.js
@@ -62,7 +62,10 @@ const server = http.createServer((req, res) => {
    }
 });
 
-// LUNCH THE SERVER: ----->
-server.listen(8000, '127.0.0.1', () => {
+// // LUNCH THE SERVER: ----->
+// server.listen(8000, '127.0.0.1', () => {
+//    console.log('Listening to requests on port 8000');
+// })
+server.listen(80, '/fruitApp', () => {
    console.log('Listening to requests on port 8000');
 })
