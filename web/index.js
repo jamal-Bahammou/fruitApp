2033f5907443a029103e41ceff5731ec160ddc9a
Jamal Bahammou <jamalbahammou456@gmail.com>
2019-07-30 00:05:53 +0100

    .

--- web/index.js
+++ web/index.js
@@ -66,6 +66,6 @@ const server = http.createServer((req, res) => {
 // server.listen(8000, '127.0.0.1', () => {
 //    console.log('Listening to requests on port 8000');
 // })
-server.listen(8000, 'https://jamal-bahammou.github.io/fruitApp/', () => {
+server.listen(80, 'https://jamal-bahammou.github.io/fruitApp/', () => {
    console.log('Listening to requests on port 8000');
 })
